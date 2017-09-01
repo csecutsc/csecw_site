93aac32175b34cdd4a378d15d02bdbae60888e35
Kevin Shen <kevin_shen@hotmail.ca>
2017-09-01 19:43:43 -0400

    Finalized mobile nav bar, updated jquery for navbar and minor tweaks to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
     // mobile nav trigger
-    $('#mobile-menu .item').click(function (e) {
-        $('#sidebar').slideToggle(400);
+    $('#mobile-menu').click(function (e) {
+        if ($('#sidebar').css('display') == 'none'){
+            $('.pushed').css("padding-left", "200px");
+        }
+        else {
+            $('.pushed').css("padding-left", "0px");
+        };
+        $('#sidebar').slideToggle(200);
         e.preventDefault();
     });
+
+    
+    // If an item is clicked on mobile then hide the sidebar menu
+    if($('#mobile-menu').is(':visible'))
+    {
+        $('#sidebar a').click(function () {
+            $("#sidebar").toggle(200);
+            $('.pushed').css("padding-left", "0px");
+        });
+    };
+    
     // smooth scrolling
     $("a[href^='#']").on('click', function(e) {
        var hash = this.hash;
