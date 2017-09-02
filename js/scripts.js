1481b89481964186b4de974e4c54e5bb30f2410e
Kevin Shen <kevin_shen@hotmail.ca>
2017-09-01 22:15:03 -0400

    Fixed image source + navbar animations

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,24 @@ $(document).ready(function () {
         else {
             $('.pushed').css("padding-left", "0px");
         };
-        $('#sidebar').slideToggle(200);
+        $('#sidebar').animate({width:'toggle'},250);
         e.preventDefault();
     });
 
-    
-    // If an item is clicked on mobile then hide the sidebar menu
-    if($('#mobile-menu').is(':visible'))
-    {
-        $('#sidebar a').click(function () {
+    // sidebar active item
+    $('#sidebar').on('click', '.item', function() {
+        if(!$(this).hasClass('dropdown')) {
+            $(this).addClass('active')
+            .siblings('.item')
+            .removeClass('active');
+      };
+      // If an item is clicked on mobile then hide the sidebar menu
+      if($('#mobile-menu').is(':visible'))
+        {
             $("#sidebar").toggle(200);
             $('.pushed').css("padding-left", "0px");
-        });
-    };
+        };
+    });
     
     // smooth scrolling
     $("a[href^='#']").on('click', function(e) {
@@ -30,14 +35,6 @@ $(document).ready(function () {
            window.location.hash = hash;
          });
         e.preventDefault();
-    });
-    // sidebar active item
-    $('#sidebar').on('click', '.item', function() {
-        if(!$(this).hasClass('dropdown')) {
-            $(this).addClass('active')
-            .siblings('.item')
-            .removeClass('active');
-      }
     });
     
     // To enable Semantic Modules
